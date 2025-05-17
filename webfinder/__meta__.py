04b7d600a1ce05d5ff81fa8a1f6b31223095db9c
Helvio Junior <helvio_junior@hotmail.com>
2025-05-17 14:54:55 -0300

    v0.1.13

--- webfinder/__meta__.py
+++ webfinder/__meta__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.12'
+__version__ = '0.1.13'
 __title__ = "Waf Web Finder"
 __description__ = "Automated web server (behind WAF) finder."
 __url__ = "https://github.com/helviojunior/webfinder"
